49970cda87440ec28d1175d674c8b6ba1e855d39
Mooka <mooka3084@gmail.com>
2023-11-22 23:50:57 +0100

    update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,11 @@
---creates a database a new user with specified permissions
+-- Create hbnb_test_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create hbnb_test user if it doesn't exist and set password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

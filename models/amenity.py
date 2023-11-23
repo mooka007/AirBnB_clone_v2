58ae096dccb8b251c52d4ba7ab6a8fa9a00dd199
Mooka <mooka3084@gmail.com>
2023-11-23 14:31:52 +0100

    task 10 update

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage_type
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
+    """ documment doc """
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+from sqlalchemy.orm import relationship
 from models.amenity import Amenity
 from models.review import Review
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.sql.schema import Table
-from sqlalchemy.orm import relationship
+from os import getenv
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
-if storage_type == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False)
-                          )
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False,
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False,
+    ),
+)
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    if storage_type == 'db':
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    """A place to stay"""
+
+    __tablename__ = "places"
+    if storage_type == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
@@ -37,10 +44,11 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
-                               cascade='all, delete, delete-orphan')
-        amenities = relationship('Amenity', secondary=place_amenity,
-                                 viewonly=False, backref='place_amenities')
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+        reviews = relationship('Review', cascade="all,delete", backref="place")
+
     else:
         city_id = ""
         user_id = ""
@@ -55,40 +63,29 @@ class Place(BaseModel, Base):
         amenity_ids = []
 
         @property
-        def reviews(self):
-            ''' returns list of review instances with place_id
-                equals to the cyrrent Place.id
-                FileStorage relationship between Place and Review
-            '''
+        def amenities(self):
+            """Getter docuemnt"""
             from models import storage
-            all_revs = storage.all(Review)
-            lst = []
-            for rev in all_revs.values():
-                if rev.place_id == self.id:
-                    lst.append(rev)
-            return lst
+            amenitiesList = []
+            amenitiesAll = storage.all(Amenity)
+            for amenity in amenitiesAll.values():
+                if amenity.id in self.amenity_ids:
+                    amenitiesList.append(amenity)
+            return amenitiesList
 
         @property
-        def amenities(self):
-            ''' returns the list of Amenity instances
-                based on the attribute amenity_ids that
-                contains all Amenity.id linked to the Place
-            '''
+        def reviews(self):
+            """Getter document"""
             from models import storage
-            all_amens = storage.all(Amenity)
-            lst = []
-            for amen in all_amens.values():
-                if amen.id in self.amenity_ids:
-                    lst.append(amen)
-            return lst
+            reviewsList = []
+            reviewsAll = storage.all(Review)
+            for review in reviewsAll.values():
+                if review.place_id in self.id:
+                    reviewsList.append(review)
+            return reviewsList
 
         @amenities.setter
-        def amenities(self, obj):
-            ''' method for adding an Amenity.id to the
-                attribute amenity_ids. accepts only Amenity
-                objects
-            '''
-            if obj is not None:
-                if isinstance(obj, Amenity):
-                    if obj.id not in self.amenity_ids:
-                        self.amenity_ids.append(obj.id)
+        def amenities(self, amenity):
+            """Setter document"""
+            if isinstance(amenity, Amenity):
+                self.amenity_ids.append(amenity.id)

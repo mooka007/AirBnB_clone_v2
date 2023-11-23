f7860b93048fd57c813efada0969f9b06fd42df2
Mooka <mooka3084@gmail.com>
2023-11-23 14:16:52 +0100

    update

--- models/user.py
+++ models/user.py
@@ -14,5 +14,3 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
